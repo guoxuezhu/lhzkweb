ded16036538b80e0d29eb372517ba985429e2683
guoxuezhu <1174327885@qq.com>
2019-04-23 15:31:35 +0800

    login

---
+++ src/components/Login.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="borde_red">
+    <h1 class="borde_red">{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      msg: 'Login'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.borde_red{
+  border: 1px solid red;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,24 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/Login'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+const routes = [
+  {
+    path: '/lelloWorld',
+    name: 'HelloWorld',
+    component: HelloWorld
+  }, {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  }
+]
+
+const router = new Router({
+  routes
 })
+
+export default router
