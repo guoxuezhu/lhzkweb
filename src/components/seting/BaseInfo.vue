985f332ce0b6a3290e8a64151cab3d742154419f
gxz <1174327885@qq.com>
2019-11-14 14:50:37 +0800

    fix

--- src/components/seting/BaseInfo.vue
+++ src/components/seting/BaseInfo.vue
@@ -13,17 +13,17 @@
         <br>
         <b-input-group>
           <b-input-group-prepend is-text><b style="width: 118px;">IP 地址</b></b-input-group-prepend>
-          <b-form-input type="text" v-model.trim="baseinfoList.zkip" placeholder="请输入IP地址"></b-form-input>
+          <b-form-input type="text" disabled v-model.trim="baseinfoList.zkip" placeholder="请输入IP地址"></b-form-input>
         </b-input-group>
         <br>
         <b-input-group>
           <b-input-group-prepend is-text><b style="width: 118px;">系统版本</b></b-input-group-prepend>
-          <b-form-input type="text" v-model.trim="baseinfoList.zkVersion" placeholder="请输入系统版本"></b-form-input>
+          <b-form-input type="text" disabled v-model.trim="baseinfoList.zkVersion" placeholder="请输入系统版本"></b-form-input>
         </b-input-group>
         <br>
         <b-input-group>
           <b-input-group-prepend is-text><b style="width: 118px;">数据库版本</b></b-input-group-prepend>
-          <b-form-input type="text" v-model.trim="baseinfoList.geendaoVersion" placeholder="请输入数据库版本"></b-form-input>
+          <b-form-input type="text" disabled v-model.trim="baseinfoList.geendaoVersion" placeholder="请输入数据库版本"></b-form-input>
         </b-input-group>
         <br>
         <b-input-group>
