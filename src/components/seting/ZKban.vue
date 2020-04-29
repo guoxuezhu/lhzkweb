2e299077335f89bf7a20ca5ff83e192b1341a594
gxz <1174327885@qq.com>
2020-04-29 17:25:45 +0800

    添加控制按钮

--- src/components/seting/ZKban.vue
+++ src/components/seting/ZKban.vue
@@ -3,7 +3,48 @@
     <br><br>
     <b-button @click="btnSendMsg('SKJAA')" v-b-popover.hover.right="'没有卡的时候可以控制操作面板进入控制界面'"
       variant="outline-success">启动操作面板</b-button>
+    <br><br>
+    <div class="borde_1">
+      <b>视频一键全切</b>
+      <br><br>
+      <b-button @click="btnSendMsg('VIDC7')" variant="outline-success" class="vid_btn">录播</b-button>
+      <b-button @click="btnSendMsg('VIDC8')" variant="outline-success" class="vid_btn">电脑</b-button>
+      <b-button @click="btnSendMsg('VIDC0')" variant="outline-success" class="vid_btn">老师投屏</b-button>
+      <b-button @click="btnSendMsg('VIDC1')" variant="outline-success" class="vid_btn">小组一</b-button>
+      <b-button @click="btnSendMsg('VIDC2')" variant="outline-success" class="vid_btn">小组二</b-button>
+      <b-button @click="btnSendMsg('VIDC3')" variant="outline-success" class="vid_btn">小组三</b-button>
+      <b-button @click="btnSendMsg('VIDC4')" variant="outline-success" class="vid_btn">小组四</b-button>
+      <b-button @click="btnSendMsg('VIDC5')" variant="outline-success" class="vid_btn">小组五</b-button>
+      <b-button @click="btnSendMsg('VIDC6')" variant="outline-success" class="vid_btn">小组六</b-button>
+    </div>
     <br>
+    <div class="borde_1">
+      <b>录播</b>
+      <br><br>
+      <b-button @click="btnSendMsg('LUB1')" variant="outline-success" class="vid_btn">开始</b-button>
+      <b-button @click="btnSendMsg('LUB2')" variant="outline-success" class="vid_btn">暂停</b-button>
+      <b-button @click="btnSendMsg('LUB3')" variant="outline-success" class="vid_btn">停止</b-button>
+      <b-button @click="btnSendMsg('LUB4')" variant="outline-success" class="vid_btn">直播</b-button>
+    </div>
+    <br>
+    <div class="borde_1">
+      <b-button @click="btnSendMsg('MBS1')" variant="outline-success" class="vid_btn">上课</b-button>
+      <b-button @click="btnSendMsg('MBS1')" variant="outline-success" class="vid_btn">下课</b-button>
+      <br><br>
+      <b-button @click="btnSendMsg('MBS13')" variant="outline-success" class="vid_btn">灯光开</b-button>
+      <b-button @click="btnSendMsg('MBS3')" variant="outline-success" class="vid_btn">窗帘开</b-button>
+      <b-button @click="btnSendMsg('MBS9')" variant="outline-success" class="vid_btn">投影机开</b-button>
+      <b-button @click="btnSendMsg('MBS11')" variant="outline-success" class="vid_btn">幕布升</b-button>
+      <b-button @click="btnSendMsg('MBS39')" variant="outline-success" class="vid_btn">空调开</b-button>
+      <br><br>
+      <b-button @click="btnSendMsg('MBS14')" variant="outline-success" class="vid_btn">灯光关</b-button>
+      <b-button @click="btnSendMsg('MBS4')" variant="outline-success" class="vid_btn">窗帘关</b-button>
+      <b-button @click="btnSendMsg('MBS10')" variant="outline-success" class="vid_btn">投影机关</b-button>
+      <b-button @click="btnSendMsg('MBS12')" variant="outline-success" class="vid_btn">幕布降</b-button>
+      <b-button @click="btnSendMsg('MBS40')" variant="outline-success" class="vid_btn">空调关</b-button>
+      <br><br>
+      <b-button @click="btnSendMsg('MJD46')" variant="outline-success" class="vid_btn">门禁</b-button>
+    </div>
   </div>
 </template>
 
@@ -74,7 +115,13 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.btn_lubo_tijiao {
+.vid_btn {
+  width: 100px;
+  margin-left: 10px;
+}
+.borde_1 {
+  border: 1px solid #0000FF;
+  padding: 30px;
   text-align: center;
 }
 </style>
